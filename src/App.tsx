52bb1db0c95a46b2e5493086155073432fdda629
web6dev <calista@web6devs-MacBook-Air.local>
2023-09-25 08:15:49 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="bg-black flex text-white w-full h-screen">
+      <h1 className="text-4xl m-auto">Hello world</h1>
+    </main>
+  );
+}
+
+export default App;
