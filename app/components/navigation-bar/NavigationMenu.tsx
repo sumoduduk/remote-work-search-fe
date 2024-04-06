4ad71786f6a968d4151edd5f9a4b94897ccd3653
sumoduduk <sumoduduk13@gmail.com>
2024-04-07 04:49:44 +0700

    fix: replace react roter dom to remix

--- app/components/navigation-bar/NavigationMenu.tsx
+++ app/components/navigation-bar/NavigationMenu.tsx
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
-
 import { NavItem } from "~/types/nav";
 import { cn } from "~/lib/utils";
+import { Link } from "@remix-run/react";
 
 export interface MainNavProps {
   items?: NavItem[];
